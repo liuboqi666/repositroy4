528d07fcfe0bc53712a533b0d346ffb7cf9fe981
liuboqi <896287963@qq.com>
2018-12-23 20:56:50 +0800

    改了

--- git_commit/src/com/itheima/git/TestGit.java
+++ git_commit/src/com/itheima/git/TestGit.java
@@ -16,5 +16,8 @@ public class TestGit {
     public  void    commit(){
         System.out.println("2222222222222222222222");
          System.out.println("2222222222222222222222");
+ System.out.println("2244444444444444444442222");
+         System.out.println("2255555555555555555555555555552222"); System.out.println("2222222222222222222222");
+         System.out.println("2222222222222222222222");
     }
 }
